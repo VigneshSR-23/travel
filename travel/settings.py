9edce3ccf46fb4270c18a793aff0c1510c209dd9
Vignesh-SASTRAUNIVERSITY <222003108@sastra.ac.in>
2021-08-05 20:39:32 +0530

    Final2 commit

--- travel/settings.py
+++ travel/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'cdb94too^35#eb=^6z0#&x%g8hk7)t2_80lwr61beb@(i9wgzc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','travello-2021.herokuapp.com']
 
 # Application definition
 
