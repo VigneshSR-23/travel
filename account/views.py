9d86900897bf5f6d43fa77e0b31c997234576380
Vignesh-SASTRAUNIVERSITY <222003108@sastra.ac.in>
2021-08-06 14:14:24 +0530

    Finals10 commit

--- account/views.py
+++ account/views.py
@@ -3,12 +3,9 @@ from django.contrib.auth.models import User,auth
 from django.contrib import messages
 from travello.models import Details
 from travello.models import Destination
-import email, smtplib, ssl
-from email import encoders
-from email.mime.base import MIMEBase
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
 # Create your views here.
+
+h=""
 def logout(request):
     auth.logout(request)
     return redirect('/')
@@ -65,7 +62,6 @@ def book(request):
     temp1=0
     temp2=""
     s=""
-    h=""
     flag2=False
     check=False
     if(request.method=='POST'):
@@ -120,7 +116,7 @@ def book(request):
                 return render(request,'book.html')
             else:
                 Details.objects.filter(email=h).update(wallet=temp1-amt)
-                Details.objects.filter(email=h).update(places=s+" , "+temp2)
+                Details.objects.filter(email=h).update(places=temp2+","+s)
             des=Details.objects.all()
             
             for p in des:
@@ -130,31 +126,6 @@ def book(request):
         else:
            messages.info(request,'** CREATE ACCOUNT FIRST AND THEN TRY TO BOOK LOCATIONS **')
            return redirect('book')
-    subject = "Travel"
-    body = "This is an email with attachment sent from Python"
-    sender_email = "travellowebsitedjango@gmail.com"
-    receiver_email = h
-    password = "vigneshsubu@"
-    message = MIMEMultipart()
-    message["From"] = sender_email
-    message["To"] = receiver_email
-    message["Subject"] = subject
-    message["Bcc"] = receiver_email
-    message.attach(MIMEText(body, "plain"))
-    filename = "abc.txt"
-    with open(filename, "rb") as attachment:
-        part = MIMEBase("application", "octet-stream")
-        part.set_payload(attachment.read())
-    encoders.encode_base64(part)
-    part.add_header(
-    "Content-Disposition",
-    f"attachment; filename= {filename}",)
-    message.attach(part)
-    text = message.as_string()
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-        server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, text) 
     return render(request,'book.html')
 
 
