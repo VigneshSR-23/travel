59b500c0ce5306762abed01ff6418849d5e106d7
Vignesh-SASTRAUNIVERSITY <222003108@sastra.ac.in>
2021-08-06 14:17:19 +0530

    admin commit

--- travello/admin.py
+++ travello/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Destination
-
+from .models import Details
 admin.site.register(Destination)
+admin.site.register(Details)
 # Register your models here.
